46d0b695499867f7f92c02e2ce3eb07d0e04fdca
dbarkelew <dbarkelew@users.noreply.github.com>
2019-11-14 16:05:57 -0700

    Revert "Clarified requirements"
    This reverts commit 2ebf5c1d39bef9a4f6059237533e414d60d5751f.

--- bin/boot-cloud-from-local.sh
+++ bin/boot-cloud-from-local.sh
@@ -10,16 +10,14 @@ AVAILABILITY_ZONE="${AVAILABILITY_ZONE:-us-west-2a}"
 # Bucket may exist.
 # Can create a bucket with something like:
 # aws s3api create-bucket --bucket vmware-hello-world-idjfuiewhj --create-bucket-configuration LocationConstraint=us-west-2
-# Bucket name must be DNS compliant
 S3_BUCKET="${S3_BUCKET:-quickstart-vmware-com}"
 
 # Will error if the bucket doesn't exist or you don't have permission to it.
 aws s3api head-bucket --bucket "${S3_BUCKET}"
 
 # If bucket cannot be found, create it.
-# If using us-east-1 then the LocationConstraint fails and needs to be removed since us-east-1 is the default.
 if [ "$?" -eq 255 ]; then
-  aws s3api create-bucket --bucket "${S3_BUCKET}" --create-bucket-configuration LocationConstraint="${REGION}" --region "${REGION}"
+  aws s3api create-bucket --bucket "${S3_BUCKET}" --create-bucket-configuration LocationConstraint=$REGION --region $REGION
 fi
 
 # Where "path/to/your/files" is the directory in S3 under which the templates and scripts directories will be placed
@@ -29,14 +27,11 @@ S3_PREFIX="${S3_PREFIX:-test-local/}"
 CNI="${CNI:-calico}"
 
 # What you want to call your CloudFormation stack
-# Member must satisfy regular expression pattern: [a-zA-Z][-a-zA-Z0-9]*
 STACK="${STACK:-my-k8s-cluster}"
 
 # What SSH key you want to allow access to the cluster (must be created ahead of time in your AWS EC2 account)
 KEYNAME="${KEYNAME:-laptop}"
 
-# Must be a valid Current Generation (non-burstable) EC2 instance type.
-# m5.large 2CPU 8GB
 INSTANCE_TYPE="${INSTANCE_TYPE:-m5.large}"
 
 # What IP addresses should be able to connect over SSH and over the Kubernetes API
@@ -45,19 +40,18 @@ INGRESS=0.0.0.0/0
 # Copy the files from your local directory into your S3 bucket
 aws s3 sync --acl=public-read ./templates "s3://${S3_BUCKET}/${S3_PREFIX}templates/"
 aws s3 sync --acl=public-read ./scripts "s3://${S3_BUCKET}/${S3_PREFIX}scripts/"
-aws s3 sync --acl=public-read ./bin "s3://${S3_BUCKET}/${S3_PREFIX}bin/"
 
 aws cloudformation create-stack \
   --region "${REGION}" \
   --stack-name "${STACK}" \
   --template-url "https://${S3_BUCKET}.s3.amazonaws.com/${S3_PREFIX}templates/kubernetes-cluster-with-new-vpc.template" \
   --parameters \
-  ParameterKey=AvailabilityZone,ParameterValue="${AVAILABILITY_ZONE}" \
-  ParameterKey=KeyName,ParameterValue="${KEYNAME}" \
-  ParameterKey=QSS3BucketName,ParameterValue="${S3_BUCKET}" \
-  ParameterKey=QSS3KeyPrefix,ParameterValue="${S3_PREFIX}" \
-  ParameterKey=AdminIngressLocation,ParameterValue="${INGRESS}" \
-  ParameterKey=NetworkingProvider,ParameterValue="${CNI}" \
-  ParameterKey=InstanceType,ParameterValue="${INSTANCE_TYPE}" \
+    ParameterKey=AvailabilityZone,ParameterValue="${AVAILABILITY_ZONE}" \
+    ParameterKey=KeyName,ParameterValue="${KEYNAME}" \
+    ParameterKey=QSS3BucketName,ParameterValue="${S3_BUCKET}" \
+    ParameterKey=QSS3KeyPrefix,ParameterValue="${S3_PREFIX}" \
+    ParameterKey=AdminIngressLocation,ParameterValue="${INGRESS}" \
+    ParameterKey=NetworkingProvider,ParameterValue="${CNI}" \
+    ParameterKey=InstanceType,ParameterValue="${INSTANCE_TYPE}" \
   --capabilities=CAPABILITY_IAM \
   --disable-rollback
